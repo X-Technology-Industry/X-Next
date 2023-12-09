fce14ae82de984c2e48a73014ef7882fc44e0a57
園洛樱介 <1937009360@qq.com>
2023-12-09 13:49:17 +0800

    updated:卡片int

--- card/Foo.tsx
+++ card/Foo.tsx
@@ -1,8 +1,7 @@
+import { cardDataINT } from "@/interface";
 import type { FC } from "react";
 
-interface FooProps {}
-
-const Foo: FC<FooProps> = () => {
+const Foo: FC<cardDataINT> = ({ data }) => {
   return (
     <section className="h-screen bg-[#212121] flex flex-col justify-center">
       <aside className="bg-black m-auto  text-white p-6 rounded-lg w-full max-w-lg font-mono ">
@@ -17,9 +16,7 @@ const Foo: FC<FooProps> = () => {
         <div className="mt-4">
           <p className="text-green-400">$ Welcom to X-Next</p>
           <p className="text-white">+ X-Next@1.0.0</p>
-          <p className="text-white">
-           Author:X-29
-          </p>
+          <p className="text-white">Author:X-29</p>
           <p className="text-green-400">$</p>
         </div>
       </aside>
